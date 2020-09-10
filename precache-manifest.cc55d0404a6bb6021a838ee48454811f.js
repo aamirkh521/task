82c3eb6ce031be088d478dee20559cfbb83506b7
aamirkh21 <mraamir8@gmail.com>
2020-09-10 14:07:33 +0530

    Updates

---
+++ precache-manifest.cc55d0404a6bb6021a838ee48454811f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624e2d2bbcc81b7111127976759d566f",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "e89b0d282f4794dcf704",
+    "url": "/task/static/css/main.e5857c85.chunk.css"
+  },
+  {
+    "revision": "5d706a4676ce1ac291ae",
+    "url": "/task/static/js/2.3e10ef85.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task/static/js/2.3e10ef85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89b0d282f4794dcf704",
+    "url": "/task/static/js/main.9616b2bb.chunk.js"
+  },
+  {
+    "revision": "6c6198df735d04dd0810",
+    "url": "/task/static/js/runtime-main.da6ea48b.js"
+  }
+]);
